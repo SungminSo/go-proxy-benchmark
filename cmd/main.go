25cb97055a738c3885f6e1d83f7eb5a6734e7f27
SungminSo <soos196@hanyang.ac.kr>
2021-01-07 18:25:45 +0900

    postman에서 proxy 설정하고 request 날렸을 때 response 주기

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,14 @@ import (
 )
 
 func main() {
+	username, password := "qwerasdf", "qwerasdf"
+
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
 
-	req.SetProxyBasic(proxy)
+	req.SetProxyBasic(proxy, func(user, pwd string) bool {
+		return user == username && password == pwd
+	})
 
 	log.Println("asdfasdfasdf")
 	log.Fatal(http.ListenAndServe(":8080", proxy))
--- req/basic.go
+++ req/basic.go
@@ -1,30 +1,32 @@
 package req
 
-
 import (
 	"github.com/elazarl/goproxy"
 	"log"
 	"net/http"
-	"time"
 )
 
-func SetProxyBasic(proxy *goproxy.ProxyHttpServer) {
+func SetProxyBasic(proxy *goproxy.ProxyHttpServer, f func(user, passwd string) bool) {
 	ip := "114.203.110.70"
 
 	proxy.OnRequest(goproxy.Not(goproxy.SrcIpIs(ip))).DoFunc(
-		func(r *http.Request, ctx *goproxy.ProxyCtx)(*http.Request, *http.Response) {
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 			log.Println("1")
-			if h,_,_ := time.Now().Clock(); h >= 8 && h <= 19 {
+			if !f("qwerqwer", "qwerqwer") {
 				return r, goproxy.NewResponse(r,
-					goproxy.ContentTypeText,http.StatusForbidden,
+					goproxy.ContentTypeText, http.StatusForbidden,
 					"Don't waste your time!")
 			}
-			return r, nil
+			return r, goproxy.NewResponse(r,
+				goproxy.ContentTypeText,
+				http.StatusOK,
+				"Asdfasdfas")
 		})
 
-	proxy.OnRequest().DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx)(*http.Request, *http.Response) {
+	proxy.OnRequest().DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 		log.Println("2")
 		r.Header.Set("X-GoProxy","yxorPoG-X")
+
 		return r, nil
 	})
 
