918f32b4faaf2fab857dbb59316383aa368ea533
SungminSo <soos196@hanyang.ac.kr>
2021-01-07 18:38:52 +0900

    goproxy 메소드에 'Func'가 붙은 것은 request 들어올 때 실행, 아닌 것은 프록시 서버 set 할 때 실행

--- req/basic.go
+++ req/basic.go
@@ -7,6 +7,20 @@ import (
 	"net/http"
 )
 
+func DoLogTest() goproxy.ReqHandler {
+	log.Println("5")
+	return goproxy.FuncReqHandler(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		return req, nil
+	})
+}
+
+func handleConnectLogTest() goproxy.HttpsHandler {
+	log.Println("4")
+	return goproxy.FuncHttpsHandler(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+		return goproxy.OkConnect, host
+	})
+}
+
 func SetProxyBasic(proxy *goproxy.ProxyHttpServer, f func(user, passwd string) bool) {
 	ip := "114.203.110.70"
 
@@ -17,11 +31,15 @@ func SetProxyBasic(proxy *goproxy.ProxyHttpServer, f func(user, passwd string) b
 		return r, nil
 	})
 
+	proxy.OnRequest().Do(DoLogTest())
+
 	proxy.OnRequest().HandleConnectFunc(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
 		fmt.Println("여기로는 안오냐ㅑㅑㅑㅑ")
 		log.Println("3")
 		return goproxy.OkConnect, host
 	})
+
+	proxy.OnRequest().HandleConnect(handleConnectLogTest())
 
 	proxy.OnRequest(goproxy.Not(goproxy.SrcIpIs(ip))).DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
