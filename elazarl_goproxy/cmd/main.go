9c9f60388f4b2ff61452c4d5605973c4b990772b
SungminSo <soos196@hanyang.ac.kr>
2021-01-08 10:47:35 +0900

    on_response에 resp stat 찍는 예시 추가

--- elazarl_goproxy/cmd/main.go
+++ elazarl_goproxy/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/SungminSo/simple-go-proxy/elazarl_goproxy/on_request"
+	"github.com/SungminSo/simple-go-proxy/elazarl_goproxy/on_response"
 	"github.com/elazarl/goproxy"
 	"log"
 	"net/http"
@@ -18,6 +19,8 @@ func main() {
 	on_request.SetProxyBasic(proxy, func(user, pwd string) bool {
 		return user == username && password == pwd
 	})
+
+	on_response.SetProxyStats(proxy)
 
 	log.Println("start proxy server at :8080")
 	log.Fatal(http.ListenAndServe(":8080", proxy))
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/SungminSo/simple-go-proxy
 
 go 1.15
 
-require github.com/elazarl/goproxy v0.0.0-20201021153353-00ad82a08272
+require (
+	github.com/elazarl/goproxy v0.0.0-20201021153353-00ad82a08272
+	github.com/elazarl/goproxy/ext v0.0.0-20190711103511-473e67f1d7d2
+)
